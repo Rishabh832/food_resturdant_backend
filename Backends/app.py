15ec74edab2e529b32a93c296d819e497f4661f5
Rishabh <rkatiyar318@gmail.com>
2025-08-12 18:40:39 +0530

    add

--- Backends/app.py
+++ Backends/app.py
@@ -60,7 +60,9 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    print("🚀 Flask is starting...")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    print(f"🚀 Flask is starting on port {port}...")
+    app.run(host="0.0.0.0", port=port)
