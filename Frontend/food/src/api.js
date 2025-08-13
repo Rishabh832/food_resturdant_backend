35d3db7452a82192ee6a66c3d750628366579c37
Rishabh <rkatiyar318@gmail.com>
2025-08-13 16:53:28 +0530

    a

--- Frontend/food/src/api.js
+++ Frontend/food/src/api.js
@@ -2,7 +2,7 @@ let API_URL = "";
 
 if (window.location.hostname === "localhost") {
   // Local development
-  API_URL = "http://localhost:5173";
+  API_URL = "http://localhost:5000";
 } else {
   // Production (Render backend)
   API_URL = "https://food-resturdant-backend-1.onrender.com";
